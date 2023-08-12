db81bcc14a189b3ab75ff142caca27fea53cdb45
Aman Yadav <86738676+amanyadav-10@users.noreply.github.com>
2023-08-12 16:11:59 +0530

    Time: 3 ms (36.42%), Space: 6.5 MB (20.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        int dp[m][n];
-        dp[0][0] = 1; //base case
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        return helper(0,0,m,n,dp);
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j] = 1;
-                }
-                
-                else{
-                    int up =0 ;
-                    int left =0;
-                    if(i>0){
-                        up = dp[i-1][j];
-                    }
-                    if(j>0){
-                        left = dp[i][j-1];
-                    }
-                    dp[i][j] = up + left;
-                    
-                }
-            }
+        
+    }
+    
+    int helper(int i,int j,int m,int n,vector<vector<int>>& dp){
+        if(i>m-1 || j>n-1){
+            return 0;
         }
-        return dp[m-1][n-1];
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        return dp[i][j] =  helper(i+1,j,m,n,dp) +helper(i,j+1,m,n,dp);
         
     }
 };
+
+// DP ON GRIDS 
