e01606801f9df16ee2dee191e515e8840e67f1cb
Aman Yadav <86738676+amanyadav-10@users.noreply.github.com>
2023-08-17 23:05:14 +0530

    Time: 15 ms (44.59%), Space: 10.1 MB (71.04%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -6,12 +6,14 @@ public:
         int m = board[0].size();
         
         for(int i=0;i<n;i++){
-            dfs(i,0,board);
-            dfs(i,m-1,board);
-        }
-        for(int j=0;j<m;j++){
-            dfs(0,j,board);
-            dfs(n-1,j,board);
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(board[i][j]=='O'){
+                        dfs(i,j,board);
+                    }
+                    
+                }
+            }
         }
         
         for(int i=0;i<n;i++){
