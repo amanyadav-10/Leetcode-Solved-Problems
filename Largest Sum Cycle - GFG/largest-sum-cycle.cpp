bd205da5c5f35cd15981ccc0a9066a04389f23bd
Aman Yadav <86738676+amanyadav-10@users.noreply.github.com>
2023-08-26 14:30:32 +0530

    Added solution - LeetHub

--- Largest Sum Cycle - GFG/largest-sum-cycle.cpp
+++ Largest Sum Cycle - GFG/largest-sum-cycle.cpp
@@ -10,57 +10,50 @@ class Solution
   public:
   long long largestSumCycle(int n, vector<int> edge)
   {
-   
-    
-    // cnt vector will count the Indegree of each Node    
-    vector<int> cnt(n,0);
-    
-    for(auto i : edge){
-        if(i != -1) cnt[i]++;
+    // make the indegree lsit here
+    vector<int> indegree(n,0);
+    for(auto x: edge){
+        if(x!=-1){
+            indegree[x]++;
+        }
     }
     
     queue<int> q;
-    
-    /*
-    
-    Fact that a Node with indegree = 0 is out of cycle.
-    We'll mark that node visited and cut that node from the graph.
-    To do so, find the edge from that node and decrease the indegree of edge[node] by 1.
-    
-    */
-    
     vector<int> vis(n,0);
-    for(int i = 0; i<n; i++){
-        if(cnt[i]==0){
-            vis[i] = 1;
+    for(int i=0;i<n;i++){
+        if(indegree[i]==0){
+            vis[i]=1;
             q.push(i);
         }
     }
     
     while(!q.empty()){
-        int node = q.front(); q.pop();
-        if(edge[node] == -1) continue;
-        --cnt[edge[node]];
-        if(cnt[edge[node]] == 0){
-            vis[edge[node]] = 1;
+        int node = q.front();
+        q.pop();
+        if(edge[node]==-1){
+            continue;
+        }
+        --indegree[edge[node]];
+        if(indegree[edge[node]]==0){
+            vis[edge[node]]=1;
             q.push(edge[node]);
         }
     }
     
     int ans = -1;
-    
-    for(int i = 0; i<n; i++){
-        if(vis[i]) continue;
-        int val = 0;
-        for(int st = i; vis[st] == 0; st = edge[st]){
-            vis[st] = 1;
-            val += st;
+    for(int i=0;i<n;i++){
+        if(vis[i]){
+            continue;
         }
-        ans = max(ans,val);
+        int val = 0;
+        for(int st=i;vis[st]==0;st=edge[st]){
+            vis[st]=1;
+            val+=st;
+        }
+        ans=max(ans,val);
     }
     
     return ans;
-    
   }
 };
 
