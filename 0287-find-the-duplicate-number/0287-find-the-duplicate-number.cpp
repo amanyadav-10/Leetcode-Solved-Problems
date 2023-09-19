a466af2c5ad4d517ee06c52eaf590d1d66ca3c57
Aman Yadav <86738676+amanyadav-10@users.noreply.github.com>
2023-09-19 18:25:54 +0530

    Time: 149 ms (20.39%), Space: 84.3 MB (14.31%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-//         marking the index negative and solve the problem 
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[abs(nums[i])-1]<0){
-                return abs(nums[i]);//we have to return the value 
+        unordered_map<int,int> mp;
+        for(auto x: nums){
+            if(mp[x]==1){
+                return x;
             }
-            else{
-                nums[abs(nums[i])-1]*=-1;
-            }
+            mp[x]++;
         }
         return 0;
+        
     }
 };
