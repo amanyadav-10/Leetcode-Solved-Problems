150f408f7fb8bf6e097e244d4f174193b5190559
Aman Yadav <86738676+amanyadav-10@users.noreply.github.com>
2024-07-31 17:30:23 +0530

    Time: 3 ms (34.14%), Space: 7.5 MB (69.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
+    int dp[101][101];
     int uniquePaths(int m, int n) {
-        
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
-        return helper(0,0,m,n,dp);
-        
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,m,n);
         
     }
     
-    int helper(int i,int j,int m,int n,vector<vector<int>>& dp){
-        if(i>m-1 || j>n-1){
-            return 0;
-        }
-        if(i==m-1 && j==n-1){
+    int helper(int i,int j,int n,int m){
+        if(i==n-1 && j==m-1){
             return 1;
         }
-        
+        if(i>=n || j>=m || i<0 || j<0){
+            return 0;
+        }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        
-        return dp[i][j] =  helper(i+1,j,m,n,dp) +helper(i,j+1,m,n,dp);
-        
+        int rt = helper(i,j+1,n,m);
+        int bt = helper(i+1,j,n,m);
+        return dp[i][j] = rt+bt;
     }
 };
-
-// DP ON GRIDS 
